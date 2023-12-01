fba014b0629bc845a0f6733e388484fff4d3104d
valerio.iani <valerio.iani@isiway.it>
2023-12-01 16:50:58 +0100

    prima commit

---
+++ src/test/java/it/yoi931/restTemplate/RestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package it.yoi931.restTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
